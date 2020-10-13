4b2cbb877b16cf86dc28dace6446af91389a4193
Steven Loria <sloria1@gmail.com>
2020-10-13 12:54:53 -0400

    0.7.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="fourmat",
-    version="0.7.0",
+    version="0.7.1",
     description="A library for batteries-included linting and autoformatting",
     url="https://github.com/4Catalyzer/fourmat",
     author="Giacomo Tagliabue",
