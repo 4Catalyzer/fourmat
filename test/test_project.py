64b56d148f618b9f0540f1893a839b3a055c01c7
Chris Carpita <ccarpita@butterflynetinc.com>
2021-09-10 17:14:05 -0400

    fix: Do not create pyproject.toml when running fourmat in subdirectory

---
+++ test/test_project.py
@@ -0,0 +1,36 @@
+import os
+from pathlib import Path
+import pytest
+
+from fourmat.lint import Project, PathContext
+
+TEST_DIR = Path(__file__).parent.resolve()
+REPO_ROOT = TEST_DIR.parent.resolve()
+
+
+@pytest.fixture
+def reset():
+    Project._PROJECT_ROOT = None
+    os.chdir(REPO_ROOT)
+
+
+def test_project(reset):
+    with PathContext(TEST_DIR):
+        assert str(Project.get_root()) == str(REPO_ROOT)
+
+
+def test_with_pyproject(reset):
+    test_project_path = (TEST_DIR / "fixture" / "test_project").resolve()
+    with PathContext(test_project_path):
+        assert str(Project.get_root()) == str(test_project_path)
+
+
+def test_fixture_dir(reset):
+    fixture = (TEST_DIR / "fixture").resolve()
+    with PathContext(fixture):
+        assert str(Project.get_root()) == str(REPO_ROOT)
+
+
+def test_system(reset):
+    with PathContext("/tmp"):
+        assert str(Project.get_root()) == "/"
